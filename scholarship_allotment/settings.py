06603e73bc44feb7a622199c0758ec3fa761c6ac
AntonioBiju <antonybiju68@gmail.com>
2025-01-29 11:54:17 +0530

    deployment size fixed

--- scholarship_allotment/settings.py
+++ scholarship_allotment/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +132,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/image/"
 MEDIA_ROOT = BASE_DIR / "static"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
