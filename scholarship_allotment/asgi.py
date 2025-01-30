66238e13bfdd93b263332772c53bffa59079a206
AntonioBiju <antonybiju68@gmail.com>
2025-01-30 14:48:13 +0530

    Fix some issue for Vercel hosting

--- scholarship_allotment/asgi.py
+++ scholarship_allotment/asgi.py
@@ -14,3 +14,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scholarship_allotment.settings")
 
 application = get_asgi_application()
+
+app = application
